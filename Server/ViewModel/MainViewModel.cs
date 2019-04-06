d4ae6afacb16ae5156937640a26d742d524c045c
pbnather <p.belczak@icloud.com>
2019-04-06 18:22:08 +0200

    Add OpenFileDialog and RadioButtons

--- Server/ViewModel/MainViewModel.cs
+++ Server/ViewModel/MainViewModel.cs
@@ -1,8 +1,27 @@
-﻿namespace Server.ViewModel
+﻿using Microsoft.Win32;
+using System.Windows.Input;
+
+namespace Server.ViewModel
 {
     class MainViewModel : AbstractViewModel
     {
+        public string SelectedFileName { get; set; }
         public MainViewModel(INavigationService ns) : base(ns) {}
+
+        private ICommand _chooseFile;
+        public ICommand ChooseFile => _chooseFile ?? (_chooseFile = new RelayCommand(OpenFilePickerDialog));
+
+        private void OpenFilePickerDialog()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            {
+                if (openFileDialog.ShowDialog() == true)
+                {
+                    SelectedFileName = openFileDialog.FileName;
+                    NotifyPropertyChanged(nameof(SelectedFileName));
+                }
+            }
+        }
 
         protected override void ExitWindow()
         {
